1422f2d6a1c975e95534e688a4532b051f02bff0
Daniel Martin <bedomartin21@gmail.com>
2026-05-29 18:48:18 -0500

    URL UI

--- src/infrastructure/http/app.ts
+++ src/infrastructure/http/app.ts
@@ -10,7 +10,7 @@ import apiRoutes from '../routes';
 dotenv.config();
 const app = express();
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: process.env.VITE_UI_URL || 'http://localhost:5173',
     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true
